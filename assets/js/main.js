7bb2f01facb0d1b0d7fa32170ed9de7553531215
sskkfmnn <sskkfmnn@gmail.com>
2023-02-16 14:57:41 +0100

    Lev1_4

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+console.log("");
+
+// window.alert("Hallo Welt");
+// window.prompt("Bitte geben Sie Ihren Namen ein");
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+// window.confirm("Stimmen Sie meiner Meinung zu?");
+
+let alter = window.prompt("Gib mir bitte dein Alter");
+console.log(alter);
+
+let b = 5;
+console.log(b);
+
+let a = b*5-3;
+console.log(a);
+
+window.alert(a);
+console.log(a);
+
+let mann = "Robert Wadlow ist der größte Mann der Welt:";
+console.log(mann);
+
+let größe = "2.72m";
+console.log(größe);
+
+window.confirm(mann + größe);
+console.log(mann + größe);
